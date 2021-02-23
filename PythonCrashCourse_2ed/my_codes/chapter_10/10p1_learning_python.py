9dbd25b289933b334bb43e6a40f377485d416fd2
gavin <xsf9728@gmail.com>
2021-02-23 20:20:35 +0800

    chapter 8 & 9 exercises

---
+++ PythonCrashCourse_2ed/my_codes/chapter_10/10p1_learning_python.py
@@ -0,0 +1,17 @@
+# Version 1
+with open("learning_python.txt") as file_object:
+    content = file_object.read()
+print("Version 1, read the entire file:\n", content)
+
+# Version 2
+with open("learning_python.txt") as file_object:
+    print("Version 2, loop over the file object:")
+    for line in file_object:
+        print(line, end='')
+
+# Version 3
+with open("learning_python.txt") as file_object:
+    lines = file_object.readlines()
+print("\nVersion 3, store lines in a list:")
+for line in lines:
+    print(line, end='')
